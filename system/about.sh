1c8733258f0079dc057fb7737a04d463fe577366
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-15 23:36:12 +0800

    Update about.sh

--- system/about.sh
+++ system/about.sh
@@ -14,12 +14,12 @@ bl='\e[36;1m'
 gl='\e[32;1m'
 clear
 echo ""
-figlet "    Moded Script"
-figlet " By JsPhantom"
+figlet "    Moded Script" | lolcat
+figlet " By JsPhantom" | lolcat
 echo -e "    \e[32;1m╔══════════════════════════════════════════════════════╗\e[0m"
 echo -e "    \e[32;1m║                   \e[36;1mINFO SCRIPT VPS \e[0m                   \e[32;1m║\e[0m"
 echo -e "    \e[32;1m╚══════════════════════════════════════════════════════╝ \e[0m"
-echo -e "             \e[031;1m> Premium Moded Script By JSphantom <\e[0m"
+echo -e "             \e[031;1m> Premium Moded Script By JSphantom <\e[0m" | lolcat
 echo -e "    \e[0;32m╔══════════════════════════════════════════════════════╗\e[0m"
 echo -e "    \e[0;32m║        \e[1;35mFor OS Debian 10 & Ubuntu 20.04 64 bit        \e[0;32m║\e[0m"
 echo -e "    \e[0;32m║        \e[1;35mFor VPS with KVM and VMWare JSphantom         \e[0;32m║\e[0m"
