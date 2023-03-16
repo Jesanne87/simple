f5558bede4f706d7092a8ef6001838b0071d59c7
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-16 12:18:42 +0800

    Update set-br.sh

--- install/set-br.sh
+++ install/set-br.sh
@@ -9,18 +9,18 @@ make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/simple/main/system/backupBot.sh"
+#wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/simple/main/system/backupBot.sh"
 wget -O backup "https://raw.githubusercontent.com/${GitUser}/simple/main/system/backup.sh"
 wget -O bckp "https://raw.githubusercontent.com/${GitUser}/simple/main/system/bckp.sh"
 wget -O restore "https://raw.githubusercontent.com/${GitUser}/simple/main/system/restore.sh"
-wget -O strt "https://raw.githubusercontent.com/${GitUser}/simple/main/system/strt.sh"
+#wget -O strt "https://raw.githubusercontent.com/${GitUser}/simple/main/system/strt.sh"
 wget -O limit-speed "https://raw.githubusercontent.com/${GitUser}/simple/main/limit-speed.sh"
-chmod +x autobackup
+#chmod +x autobackup
 chmod +x backup
 chmod +x bckp
 chmod +x restore
-chmod +x strt
+#chmod +x strt
 chmod +x limit-speed
-chmod +x clear-log
+#chmod +x clear-log
 cd
 rm -f /root/set-br.sh
