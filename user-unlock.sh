76bf53927364dc03d1bdf2f1bd825bd5fb689e29
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-17 12:27:09 +0800

    Update user-unlock.sh

--- user-unlock.sh
+++ user-unlock.sh
@@ -27,17 +27,12 @@ exit 0
 fi
 echo -e "\e[32mloading...\e[0m"
 clear
-echo " "
-echo " "
-echo " "
 read -p "Input USERNAME to unlock: " username
 egrep "^$username" /etc/passwd >/dev/null
 if [ $? -eq 0 ]; then
 # proses mengganti passwordnya
 passwd -u $username
 clear
-  echo " "
-  echo " "
   echo " "
   echo "-------------------------------------------"
   echo -e "Username ${blue}$username${NC} successfully ${green}UNLOCKED${NC}."
@@ -46,6 +41,6 @@ clear
 else
 echo " "
 echo -e "Username ${red}$username${NC} not found in your server."
-echo " "    
-	exit 1
+    sleep 1.5
+    ssh 
 fi
