4e0e25c12b19e90ebc47557c220d319b6d0faf9d
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-17 15:47:22 +0800

    Update run-update.sh

--- update/run-update.sh
+++ update/run-update.sh
@@ -28,11 +28,9 @@ Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_p
 Info1="${Green_font_prefix}($version)${Font_color_suffix}"
 Info2="${Green_font_prefix}(LATEST VERSION)${Font_color_suffix}"
 Error="Version ${Green_font_prefix}[$ver]${Font_color_suffix} available${Red_font_prefix}[Please Update]${Font_color_suffix}"
-version=$(cat /home/ver)
-new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf | grep $version )
 #Status Version
-if [ $version = $new_version ]; then
-sts="${Info2}"
+if [ $version = $ver ]; then
+sts="${Info1}"
 else
 sts="${Error}"
 fi
@@ -59,12 +57,10 @@ version=$(cat /home/ver)
 new_version=$( curl https://raw.githubusercontent.com/${GitUser}/version/main/version.conf | grep $version )
 if [ $version = $new_version ]; then
 clear
-echo ""
 echo -e "\e[1;31mChecking New Version, Please Wait...!\e[m"
 sleep 3
 clear
 echo -e "\e[1;31mUpdate Not Available\e[m"
-echo ""
 clear
 sleep 1
 echo -e "\e[1;36mYou Have The Latest Version\e[m"
@@ -74,46 +70,42 @@ update
 fi
 clear
 echo -e "\e[1;31mUpdate Available Now..\e[m"
-echo -e ""
 sleep 2
 echo -e "\e[1;36mStart Update For New Version, Please Wait..\e[m"
 sleep 2
 clear
 echo -e "\e[0;32mGetting New Version Script..\e[0m"
 sleep 1
-echo ""
 # UPDATE RUN-UPDATE
 cd /usr/bin
-wget -O run-update "https://raw.githubusercontent.com/${GitUser}/lanjut/main/update/run-update.sh"
+wget -O run-update "https://raw.githubusercontent.com/${GitUser}/simple/main/update/run-update.sh"
 chmod +x run-update
 # RUN UPDATE
-echo ""
 clear
 echo -e "\e[0;32mPlease Wait...!\e[0m"
 sleep 6
 clear
-echo ""
 echo -e "\e[0;32mNew Version Downloading started!\e[0m"
 sleep 2
 cd /usr/bin
-wget -O update "https://raw.githubusercontent.com/${GitUser}/lanjut/main/update/update.sh"
-wget -O run-update "https://raw.githubusercontent.com/${GitUser}/lanjut/main/update/run-update.sh"
-wget -O message-ssh "https://raw.githubusercontent.com/${GitUser}/lanjut/main/update/message-ssh.sh"
-wget -O change-port "https://raw.githubusercontent.com/${GitUser}/lanjut/main/change.sh"
-wget -O system "https://raw.githubusercontent.com/${GitUser}/lanjut/main/menu/system.sh"
-wget -O menu "https://raw.githubusercontent.com/${GitUser}/lanjut/main/menu.sh"
-wget -O add-host "https://raw.githubusercontent.com/${GitUser}/lanjut/main/system/add-host.sh"
-wget -O check-sc "https://raw.githubusercontent.com/${GitUser}/lanjut/main/system/running.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/${GitUser}/lanjut/main/cert.sh"
-wget -O trojaan "https://raw.githubusercontent.com/${GitUser}/lanjut/main/menu/trojaan.sh"
-wget -O xraay "https://raw.githubusercontent.com/${GitUser}/lanjut/main/menu/xraay.sh"
-wget -O xp "https://raw.githubusercontent.com/${GitUser}/lanjut/main/xp.sh"
-wget -O port-xray "https://raw.githubusercontent.com/${GitUser}/lanjut/main/change-port/port-xray.sh"
-wget -O themes "https://raw.githubusercontent.com/${GitUser}/lanjut/main/menu/themes.sh"
-#wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/lanjut/main/system/backupBot.sh"
-wget -O backup "https://raw.githubusercontent.com/${GitUser}/lanjut/main/system/backup.sh"
-#wget -O bckp "https://raw.githubusercontent.com/${GitUser}/lanjut/main/system/bckp.sh"
-wget -O restore "https://raw.githubusercontent.com/${GitUser}/lanjut/main/system/restore.sh"
+wget -O update "https://raw.githubusercontent.com/${GitUser}/simple/main/update/update.sh"
+wget -O run-update "https://raw.githubusercontent.com/${GitUser}/simple/main/update/run-update.sh"
+wget -O message-ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/update/message-ssh.sh"
+wget -O change-port "https://raw.githubusercontent.com/${GitUser}/simple/main/change.sh"
+wget -O system "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/system.sh"
+wget -O menu "https://raw.githubusercontent.com/${GitUser}/simple/main/menu.sh"
+wget -O add-host "https://raw.githubusercontent.com/${GitUser}/simple/main/system/add-host.sh"
+wget -O check-sc "https://raw.githubusercontent.com/${GitUser}/simple/main/system/running.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/${GitUser}/simple/main/cert.sh"
+wget -O trojaan "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/trojaan.sh"
+wget -O xraay "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/xraay.sh"
+wget -O xp "https://raw.githubusercontent.com/${GitUser}/simple/main/xp.sh"
+wget -O port-xray "https://raw.githubusercontent.com/${GitUser}/simple/main/change-port/port-xray.sh"
+wget -O themes "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/themes.sh"
+#wget -O autobackup "https://raw.githubusercontent.com/${GitUser}/simple/main/system/backupBot.sh"
+wget -O backup "https://raw.githubusercontent.com/${GitUser}/simple/main/system/backup.sh"
+#wget -O bckp "https://raw.githubusercontent.com/${GitUser}/simple/main/system/bckp.sh"
+wget -O restore "https://raw.githubusercontent.com/${GitUser}/simple/main/system/restore.sh"
 chmod +x update
 chmod +x run-update
 chmod +x message-ssh
@@ -150,10 +142,10 @@ echo "$ver" > /home/ver
 rm -f update.sh
 clear
 echo -e "\e[$line┌──────────────────────────────────────────────────────┐\e[m"
-echo -e "\e[$line│                 \e[$box SCRIPT UPDATED\e[30m                  \e[$line│\e[m"
+echo -e "\e[$line│                   \e[$box SCRIPT UPDATED\e[30m                    \e[$line│\e[m"
 echo -e "\e[$line└──────────────────────────────────────────────────────┘\e[m"
 echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
+read -n 1 -s -r -p "            Press any key to back on menu"
 menu
 ;;
 2)
