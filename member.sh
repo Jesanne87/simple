8eaeb7806de2e9f2568703b20db0eca486d808f1
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-17 12:11:22 +0800

    Update member.sh

--- member.sh
+++ member.sh
@@ -1,5 +1,18 @@
-#wget https://github.com/${GitUser}/
+#!/bin/bash
+wget https://github.com/${GitUser}/
 GitUser="Jesanne87"
+# // Export Color & Information
+export WH='\033[1;37m'                   
+export RED='\033[0;31m'
+export GRN='\033[0;32m'
+export YLW='\033[0;33m'
+export BLU='\033[0;34m'
+export PLE='\033[0;35m'
+export CYA='\033[0;36m'
+export LHT='\033[0;37m'
+export NC='\033[0m'
+# LINE COLOUR
+line=$(cat /etc/line)
 #IZIN SCRIPT
 MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
@@ -27,9 +40,9 @@ exit 0
 fi
 echo -e "\e[32mloading...\e[0m"
 clear
-echo "---------------------------------------------------"
+echo "•─────────────────────────────────────────────•"
 echo "USERNAME          EXP DATE          STATUS"
-echo "---------------------------------------------------"
+echo "•─────────────────────────────────────────────•"
 while read expired
 do
 AKUN="$(echo $expired | cut -d: -f1)"
@@ -38,13 +51,17 @@ exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
 status="$(passwd -S $AKUN | awk '{print $2}' )"
 if [[ $ID -ge 1000 ]]; then
 if [[ "$status" = "L" ]]; then
-printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${RED}LOCKED${NORMAL}"
+printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "         LOCKED${NORMAL}"
 else
 printf "%-17s %2s %-17s %2s \n" "$AKUN" "$exp     " "${GREEN}UNLOCKED${NORMAL}"
 fi
 fi
 done < /etc/passwd
 JUMLAH="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
-echo "---------------------------------------------------"
+echo "•─────────────────────────────────────────────•"
 echo "Account number: $JUMLAH user"
-echo "---------------------------------------------------"
+echo "•─────────────────────────────────────────────•"
+echo ""
+read -n1 -r -p "Press any key to continue..."
+    sleep 1
+    ssh
