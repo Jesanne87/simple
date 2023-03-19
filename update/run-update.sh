bae2e86dd3602fc7123490a4bb0b43fdfa54ac06
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-19 22:04:20 +0800

    Update run-update.sh

--- update/run-update.sh
+++ update/run-update.sh
@@ -76,10 +76,6 @@ sleep 2
 clear
 echo -e "\e[0;32mGetting New Version Script..\e[0m"
 sleep 1
-# UPDATE RUN-UPDATE
-cd /usr/bin
-wget -O run-update "https://raw.githubusercontent.com/${GitUser}/simple/main/update/run-update.sh"
-chmod +x run-update
 # RUN UPDATE
 clear
 echo -e "\e[0;32mPlease Wait...!\e[0m"
@@ -105,6 +101,40 @@ rm -f port-xray
 rm -f themes
 rm -f backup
 rm -f restore
+rm -f add-host
+rm -f about
+rm -f add-ssh
+rm -f trial
+rm -f del-ssh
+rm -f member
+rm -f delete
+rm -f cek-ssh
+rm -f restart
+rm -f speedtest
+rm -f info
+rm -f renew-ssh
+rm -f autokill
+rm -f ceklim
+rm -f tendang
+rm -f clean-ram
+rm -f port-ovpn
+rm -f port-ssl
+rm -f port-squid
+rm -f port-websocket
+rm -f user-list
+rm -f user-lock
+rm -f user-unlock
+rm -f user-password
+rm -f antitorrent
+rm -f cfa
+rm -f cfd
+rm -f cfp
+rm -f check-sc
+rm -f ssh
+rm -f autoreboot
+rm -f port-ohp
+rm -f dns
+rm -f nf
 wget -O update "https://raw.githubusercontent.com/${GitUser}/simple/main/update/update.sh"
 wget -O run-update "https://raw.githubusercontent.com/Jesanne87/simple/main/update/run-update.sh"
 wget -O message-ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/update/message-ssh.sh"
@@ -125,6 +155,46 @@ wget -O themes "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/th
 wget -O backup "https://raw.githubusercontent.com/${GitUser}/simple/main/system/backup.sh"
 #wget -O bckp "https://raw.githubusercontent.com/${GitUser}/simple/main/system/bckp.sh"
 wget -O restore "https://raw.githubusercontent.com/${GitUser}/simple/main/system/restore.sh"
+wget -O add-host "https://raw.githubusercontent.com/${GitUser}/simple/main/system/add-host.sh"
+wget -O about "https://raw.githubusercontent.com/${GitUser}/simple/main/system/about.sh"
+wget -O add-ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/add-user/add-ssh.sh"
+wget -O trial "https://raw.githubusercontent.com/${GitUser}/simple/main/add-user/trial.sh"
+wget -O del-ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/delete-user/del-ssh.sh"
+wget -O member "https://raw.githubusercontent.com/${GitUser}/simple/main/member.sh"
+wget -O delete "https://raw.githubusercontent.com/${GitUser}/simple/main/delete-user/delete.sh"
+wget -O cek-ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/cek-user/cek-ssh.sh"
+wget -O restart "https://raw.githubusercontent.com/${GitUser}/simple/main/system/restart.sh"
+wget -O speedtest "https://raw.githubusercontent.com/${GitUser}/simple/main/system/speedtest_cli.py"
+wget -O info "https://raw.githubusercontent.com/${GitUser}/simple/main/system/info.sh"
+#wget -O ram "https://raw.githubusercontent.com/${GitUser}/simple/main/system/ram.sh"
+wget -O renew-ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/renew-user/renew-ssh.sh"
+wget -O autokill "https://raw.githubusercontent.com/${GitUser}/simple/main/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/${GitUser}/simple/main/cek-user/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/${GitUser}/simple/main/tendang.sh"
+wget -O clean-ram "https://raw.githubusercontent.com/${GitUser}/simple/main/clean-ram.sh"
+wget -O port-ovpn "https://raw.githubusercontent.com/${GitUser}/simple/main/change-port/port-ovpn.sh"
+wget -O port-ssl "https://raw.githubusercontent.com/${GitUser}/simple/main/change-port/port-ssl.sh"
+wget -O port-squid "https://raw.githubusercontent.com/${GitUser}/simple/main/change-port/port-squid.sh"
+wget -O port-websocket "https://raw.githubusercontent.com/${GitUser}/simple/main/change-port/port-websocket.sh"
+#wget -O wbmn "https://raw.githubusercontent.com/${GitUser}/simple/main/webmin.sh"
+#wget -O kernel-updt "https://raw.githubusercontent.com/${GitUser}/simple/main/kernel.sh"
+wget -O user-list "https://raw.githubusercontent.com/${GitUser}/simple/main/more-option/user-list.sh"
+wget -O user-lock "https://raw.githubusercontent.com/${GitUser}/simple/main/more-option/user-lock.sh"
+wget -O user-unlock "https://raw.githubusercontent.com/${GitUser}/simple/main/more-option/user-unlock.sh"
+wget -O user-password "https://raw.githubusercontent.com/${GitUser}/simple/main/more-option/user-password.sh"
+wget -O antitorrent "https://raw.githubusercontent.com/${GitUser}/simple/main/more-option/antitorrent.sh"
+wget -O cfa "https://raw.githubusercontent.com/${GitUser}/simple/main/cloud/cfa.sh"
+wget -O cfd "https://raw.githubusercontent.com/${GitUser}/simple/main/cloud/cfd.sh"
+wget -O cfp "https://raw.githubusercontent.com/${GitUser}/simple/main/cloud/cfp.sh"
+#wget -O swap "https://raw.githubusercontent.com/${GitUser}/simple/main/swapkvm.sh"
+wget -O check-sc "https://raw.githubusercontent.com/${GitUser}/simple/main/system/running.sh"
+wget -O ssh "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/ssh.sh"
+wget -O autoreboot "https://raw.githubusercontent.com/${GitUser}/simple/main/system/autoreboot.sh"
+#wget -O bbr "https://raw.githubusercontent.com/${GitUser}/simple/main/system/bbr.sh"
+wget -O port-ohp "https://raw.githubusercontent.com/${GitUser}/simple/main/change-port/port-ohp.sh"
+#wget -O panel-domain "https://raw.githubusercontent.com/${GitUser}/simple/main/menu/panel-domain.sh"
+wget -O dns "https://raw.githubusercontent.com/${GitUser}/simple/main/system/dns.sh"
+wget -O nf "https://raw.githubusercontent.com/${GitUser}/simple/main/system/nf.sh"
 chmod +x update
 chmod +x run-update
 chmod +x message-ssh
@@ -145,6 +215,40 @@ chmod +x themes
 chmod +x backup
 #chmod +x bckp
 chmod +x restore
+chmod +x add-host
+chmod +x about
+chmod +x add-ssh
+chmod +x trial
+chmod +x del-ssh
+chmod +x member
+chmod +x delete
+chmod +x cek-ssh
+chmod +x restart
+chmod +x speedtest
+chmod +x info
+chmod +x renew-ssh
+chmod +x autokill
+chmod +x ceklim
+chmod +x tendang
+chmod +x clean-ram
+chmod +x port-ovpn
+chmod +x port-ssl
+chmod +x port-squid
+chmod +x port-websocket
+chmod +x user-list
+chmod +x user-lock
+chmod +x user-unlock
+chmod +x user-password
+chmod +x antitorrent
+chmod +x cfa
+chmod +x cfd
+chmod +x cfp
+chmod +x check-sc
+chmod +x ssh
+chmod +x autoreboot
+chmod +x port-ohp
+chmod +x dns
+chmod +x nf
 clear
 echo -e ""
 echo -e "\e[0;32mDownloaded successfully!\e[0m"
@@ -160,7 +264,6 @@ echo ""
 echo -e "\e[0;32mSucces Update Script For New Version\e[0m"
 cd
 echo "$ver" > /home/ver
-rm -f update.sh
 clear
 echo -e "\e[$line┌──────────────────────────────────────────────────────┐\e[m"
 echo -e "\e[$line│                   \e[$box SCRIPT UPDATED\e[30m                    \e[$line│\e[m"
