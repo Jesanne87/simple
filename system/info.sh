4046d2fa5a5af46d4a267879e0023d419c6d2202
UltraGaming <119119765+Jesanne87@users.noreply.github.com>
2023-03-19 14:29:47 +0800

    Update info.sh

--- system/info.sh
+++ system/info.sh
@@ -22,66 +22,64 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 clear
-echo -e "\e[1;32m══════════════════ \e[93mAutoscript PREMIUM\e[1;32m ══════════════════\e[0m" | tee -a log-install.txt
+echo -e "\e[1;32m══════════════════ \e[93mAutoscript PREMIUM\e[1;32m ══════════════════\e[0m"
 echo ""
-echo ""  | tee -a log-install.txt
-echo "   >>> Service & Port"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "    [INFORMASI SSH & OpenVPN]" | tee -a log-install.txt
-echo "    -------------------------" | tee -a log-install.txt
-echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
-echo "   - OpenVPN                 : TCP 1194, UDP 2200"  | tee -a log-install.txt
-echo "   - OpenVPN SSL             : 110"  | tee -a log-install.txt
-echo "   - Stunnel4                : 222, 777"  | tee -a log-install.txt
-echo "   - Dropbear                : 143, 109"  | tee -a log-install.txt
-echo "   - OHP Dropbear            : 8585"  | tee -a log-install.txt
-echo "   - OHP SSH                 : 8686"  | tee -a log-install.txt
-echo "   - OHP OpenVPN             : 8787"  | tee -a log-install.txt
-echo "   - Websocket SSH(HTTP)     : 80"  | tee -a log-install.txt
-echo "   - Websocket SSL(HTTPS)    : 443, 2096"  | tee -a log-install.txt
-echo "   - Websocket OpenVPN       : 2097"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "    [INFORMASI Sqd, Bdvp, Ngnx]" | tee -a log-install.txt
-echo "    ---------------------------" | tee -a log-install.txt
-echo "   - Squid Proxy             : 3128, 8000 (limit to IP Server)"  | tee -a log-install.txt
-echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
-echo "   - Nginx                   : 81"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "    [INFORMASI XRAY]"  | tee -a log-install.txt
-echo "    ----------------" | tee -a log-install.txt
-echo "   - Xray Vmess Ws Tls       : 443"  | tee -a log-install.txt
-echo "   - Xray Vless Ws Tls       : 443"  | tee -a log-install.txt
-echo "   - Xray Trojan Ws Tls      : 443"  | tee -a log-install.txt
-echo "   - Xray Vless Tcp Xtls     : 443"  | tee -a log-install.txt
-echo "   - Xray Trojan Tcp Tls     : 443"  | tee -a log-install.txt
-echo "   - Xray Vmess Ws None Tls  : 80"  | tee -a log-install.txt
-echo "   - Xray Vless Ws None Tls  : 80"  | tee -a log-install.txt
-echo "   - Xray Trojan Ws None Tls : 80"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "    [INFORMASI CLASH FOR ANDROID (YAML)]"  | tee -a log-install.txt
-echo "    -----------------------------------" | tee -a log-install.txt
-echo "   - Xray Vmess Ws Yaml      : Yes"  | tee -a log-install.txt
-echo "   - Xray Vless Ws Yaml      : Yes"  | tee -a log-install.txt
-echo "   - Xray Trojan Ws Yaml     : Yes"  | tee -a log-install.txt
-echo "   --------------------------------------------------------------" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
-echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
-echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
-echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
-echo "   - Autoreboot On 05.00 GMT +8" | tee -a log-install.txt
-echo "   - Autobackup Data" | tee -a log-install.txt
-echo "   - Restore Data" | tee -a log-install.txt
-echo "   - Auto Delete Expired Account" | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
-echo "   - White Label" | tee -a log-install.txt
-echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo "   >>> Service & Port"
 echo ""
-echo -e "\e[1;32m═══════════════ \e[93mAutoscript Moded By JSphantom\e[1;32m ════════════════\e[0m" | tee -a log-install.txt
+echo "    [INFORMASI SSH & OpenVPN]"
+echo "    -------------------------"
+echo "   - OpenSSH                 : 22"
+echo "   - OpenVPN                 : TCP 1194, UDP 2200"
+echo "   - OpenVPN SSL             : 110"
+echo "   - Stunnel4                : 222, 777"
+echo "   - Dropbear                : 143, 109"
+echo "   - OHP Dropbear            : 8585"
+echo "   - OHP SSH                 : 8686"
+echo "   - OHP OpenVPN             : 8787"
+echo "   - Websocket SSH(HTTP)     : 80"
+echo "   - Websocket SSL(HTTPS)    : 443, 2096"
+echo "   - Websocket OpenVPN       : 2097"
+echo ""
+echo "    [INFORMASI Sqd, Bdvp, Ngnx]"
+echo "    ---------------------------"
+echo "   - Squid Proxy             : 3128, 8000 (limit to IP Server)"
+echo "   - Badvpn                  : 7100, 7200, 7300"
+echo "   - Nginx                   : 81"
+echo "" 
+echo "    [INFORMASI XRAY]" 
+echo "    ----------------"
+echo "   - Xray Vmess Ws Tls       : 443" 
+echo "   - Xray Vless Ws Tls       : 443" 
+echo "   - Xray Trojan Ws Tls      : 443"  
+echo "   - Xray Vless Tcp Xtls     : 443"
+echo "   - Xray Trojan Tcp Tls     : 443"
+echo "   - Xray Vmess Ws None Tls  : 80"
+echo "   - Xray Vless Ws None Tls  : 80"
+echo "   - Xray Trojan Ws None Tls : 80"
+echo ""  
+echo "    [INFORMASI CLASH FOR ANDROID (YAML)]"  
+echo "    -----------------------------------"
+echo "   - Xray Vmess Ws Yaml      : Yes"
+echo "   - Xray Vless Ws Yaml      : Yes"
+echo "   - Xray Trojan Ws Yaml     : Yes"
+echo "   --------------------------------------------------------------"
+echo "" 
+echo "   >>> Server Information & Other Features"  
+echo "   - Timezone                : Asia/Kuala_Lumpur (GMT +8)"  
+echo "   - Fail2Ban                : [ON]" 
+echo "   - Dflate                  : [ON]" 
+echo "   - IPtables                : [ON]" 
+echo "   - Auto-Reboot             : [ON]"
+echo "   - IPv6                    : [OFF]"
+echo "   - Autoreboot On 05.00 GMT +8" 
+echo "   - Autobackup Data"
+echo "   - Restore Data"
+echo "   - Auto Delete Expired Account"
+echo "   - Full Orders For Various Services"
+echo "   - White Label"
+echo ""
+echo -e "\e[1;32m══════════════════ \e[93mAutoscript By JSphantom\e[1;32m ══════════════════\e[0m"
 echo ""
 read -n1 -r -p "                Press any key to continue..."
     sleep 1
-    system
+    menu
